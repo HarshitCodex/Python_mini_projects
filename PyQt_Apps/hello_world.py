98065aa81fa069ac54a02c613625e02a58854018
HarshitCodex <harshitagrawal1971@gmail.com>
2020-04-01 04:38:13 +0530

    Create a hello world window

---
+++ PyQt_Apps/hello_world.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5 import QtGui, QtWidgets
+
+
+def window():
+    app = QtWidgets.QApplication(sys.argv)
+    w = QtWidgets.QWidget()
+    b = QtWidgets.QLabel(w)
+    b.setText("Hello World, I am Harshit! My first in PyQt5")
+    w.setGeometry(200, 200, 500, 50)  # x,y,width, height
+    b.move(50, 20)
+    w.setWindowTitle("PyQt5 Prog_1")
+    w.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    window()
