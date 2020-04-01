bd273d96f7011892ead00e563156e5b0069b8c43
HarshitCodex <harshitagrawal1971@gmail.com>
2020-04-01 05:56:31 +0530

    Get Date and Time Information

--- PyQt_Apps/Date_Time.py
+++ PyQt_Apps/Date_Time.py
@@ -9,3 +9,11 @@ print(datetime.toString())
 time = QTime.currentTime()
 
 print(time.toString(Qt.DefaultLocaleLongDate))
+print("\n")
+print("Local datetime: ", datetime.toString(Qt.DefaultLocaleLongDate))
+print("Universal Time : ", datetime.toUTC().toString(Qt.DefaultLocaleLongDate))
+
+print("The offset from UTC is : {} seconds".format(datetime.offsetFromUtc()))
+print("\n")
+print("Days in month: {}".format(now.daysInMonth()))
+print("Days in year: {}".format(now.daysInYear()))
